731fed1e15395420b35323a9bcc3b360d73a5032
sadah00 <kimberlysada30@gmail.com>
2025-09-24 01:29:13 +0300

    index.html

--- main.py
+++ main.py
@@ -6,5 +6,20 @@ app = Flask(__name__)
 def home():  
     return render_template("index.html")
 
+@app.route('/contact') 
+def contact():  
+    return render_template("contact.html")
+
+@app.route('/login') 
+def login():  
+    return render_template("login.html")
+
+@app.route('/register') 
+def register():  
+    return render_template("register.html")
+
+@app.route('/shop') 
+def shop():  
+    return render_template("shop.html")
 
 app.run(debug=True)
