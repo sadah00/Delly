2de27970af6257f54ad6f174eeadb3b121a903ff
sadah00 <kimberlysada30@gmail.com>
2025-10-06 15:23:45 +0300

    main.py

--- database.py
+++ database.py
@@ -3,3 +3,13 @@ import psycopg2
 
 conn = psycopg2.connect(host="localhost", port="5432", user="postgres", password="kimysada6",dbname="delly")
 cur=conn.cursor()
+
+def insert_user(user_details):
+    cur.execute(f"INSERT INTO users (name,email,phone_number,password) VALUES{user_details}")
+    conn.commit()
+
+
+def check_user(email):
+    cur.execute("select * from users where email = %s",(email,))
+    user = cur.fetchone()   
+    return user
--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,redirect ,url_for,flash,session
+from database import insert_user,check_user
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
+
+bcrypt = Bcrypt(app)
+
+app.secret_key = 'delicate'
 
 @app.route('/') 
 def home():  
@@ -17,10 +23,39 @@ def login():
         password = request.form['password']
 
         
+        registered_user = check_user(email)
+        print(registered_user)
+        if not registered_user:
+            flash("User doesn't exist,Please Register","danger")
+            return redirect(url_for('register'))
+        else:
+            if bcrypt.check_password_hash(registered_user[-1],password):
+                flash("Logged in Successfully","Success")
+                session["email"]=email
+                return redirect(url_for("shop"))
+            else:
+                flash("Password incorrect,try again","danger")
+        
     return render_template("login.html")
 
-@app.route('/register') 
-def register():  
+@app.route('/register',methods=['GET', 'POST']) 
+def register(): 
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        phone_number = request.form['phone_number']
+        password = request.form['password']
+        existing_user = check_user(email)
+
+        if not existing_user:
+            hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+            new_user = (name,email,phone_number,hashed_password)
+            insert_user(new_user)
+            flash("User registered successfully","success")
+            return redirect(url_for('register'))
+        
+        else:
+            flash("User already exists,please login","danger") 
     return render_template("register.html")
 
 @app.route('/shop') 
